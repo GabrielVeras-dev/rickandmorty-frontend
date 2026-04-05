d0f79763f25274b2edc2a337deef35e5a8d9e09a
Gabriel Veras <bielveras.e@hotmail.com>
2026-04-05 17:18:53 -0300

    feat: adicionado página sobre o projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PersonagemLista } from './components/personagem-lista/personagem-lista';
 import { LocalLista } from './components/local-lista/local-lista';
+import { Sobre } from './components/sobre/sobre';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'personagens', pathMatch: 'full' },
   { path: 'personagens', component: PersonagemLista },
   { path: 'locais', component: LocalLista },
+  { path: 'sobre', component: Sobre },
 ];
---
+++ src/app/components/sobre/sobre.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sobre',
+  imports: [CommonModule],
+  templateUrl: './sobre.html',
+  styleUrl: './sobre.css',
+})
+export class Sobre {
+  expiracao = new Date('2026-05-04');
+  hoje = new Date();
+  diasRestantes = Math.ceil(
+    (this.expiracao.getTime() - this.hoje.getTime()) / (1000 * 60 * 60 * 24)
+  );
+}
